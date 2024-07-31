8984abc29363252fe9a21ae7a7a67e471db8ad6c
vhoudebine <vhoudebine@microsoft.com>
2024-07-31 12:19:20 -0400

    added support for images

--- app.py
+++ app.py
@@ -2,27 +2,31 @@ import streamlit as st
 from datetime import datetime
 import requests
 from azure.ai.inference import ChatCompletionsClient
-from azure.ai.inference.models import SystemMessage, UserMessage
+from azure.ai.inference.models import SystemMessage, UserMessage, TextContentItem, ImageContentItem, ImageUrl, ImageDetailLevel
 from streamlit.runtime.scriptrunner import add_script_run_ctx
 from streamlit.runtime.scriptrunner.script_run_context import get_script_run_ctx
 from azure.core.credentials import AzureKeyCredential
 import time
-import utils
+import ai_utils
 import threading
 from threading import current_thread
+import base64
+from io import StringIO
+
 
 
 results = [None, None]
 threadErrors=[]
+base64_url = None
 ### Utility functions
 def ai_inference_stream(ctx, 
                         prompt, 
-                        index):
+                        index,
+                        uploaded_img):
     add_script_run_ctx(threading.current_thread(), ctx)
     deployment=models[index]["deployment"]
     endpoint = models[index]["endpoint"]+"/openai/deployments/"+deployment
     api_key = models[index]["key"]
-    
     if models[index]["model_type"] != "AOAI":
         client = ChatCompletionsClient(
             endpoint=models[index]["endpoint"],
@@ -39,10 +43,15 @@ def ai_inference_stream(ctx,
         
     start_time = datetime.now()
     try:
+        usercontent = [TextContentItem(text=prompt)]
+        if uploaded_img is not None:
+            usercontent.append(ImageContentItem(
+                            image_url=ImageUrl(url=uploaded_img))
+                            )
         stream = client.complete(
             messages=[
                 SystemMessage(content=st.session_state["system_prompt"]),
-                UserMessage(content=prompt),
+                UserMessage(content=usercontent),
             ],
             stream=True,
             model_extras={} if st.session_state["num_tokens"]=="" else {"max_tokens":int(st.session_state["num_tokens"])},
@@ -71,9 +80,10 @@ def ai_inference_stream(ctx,
         results[index]= {"response": assistant_reply,
                 "total_time":end_time-start_time,
                 "time_first_token":first_token-start_time,
-                "output_tokens":utils.num_tokens_from_string(assistant_reply, "gpt-4o")}
+                "output_tokens":ai_utils.num_tokens_from_string(assistant_reply, "gpt-4o")}
     except Exception as e:
         threadErrors.append([repr(e), current_thread().name])
+        print(e)
 
 
 def reset_conversation():
@@ -83,6 +93,8 @@ def reset_conversation():
     col1.empty()
     col2.empty()
 
+
+
 st.set_page_config(page_title="Model Comparison", 
                    layout="wide",
                    initial_sidebar_state="expanded")
@@ -90,8 +102,7 @@ st.title('Azure AI - LLM Comparison')
 
 headcol1, headcol2=st.columns([0.7,3])
 clear = headcol1.button('New Comparison 🔄', on_click=reset_conversation)
-prompt_container = headcol2.empty()
-
+prompt_container = headcol2.container()
 
 col1, col2 = st.columns(2)
 
@@ -110,49 +121,6 @@ with col2:
     container2 = st.container(height=300)
     timer2 = st.markdown("#### ⏱️ Timer: 0:00:00")
         
-timers = [timer1, timer2]
-containers = [container1, container2]
-columns = [col1, col2]
-missing = False
-
-if prompt := st.chat_input("Enter a prompt"):
-    prompt_container.empty()
-    prompt_container.markdown(f" **Prompt:** {prompt}")
-    models = [{"endpoint":st.session_state[f"url_{i+1}"],
-               "key":st.session_state[f"key_{i+1}"],
-               "deployment":st.session_state[f"deployment_{i+1}"],
-               "model":st.session_state[f"model_{i+1}"],
-               "model_type":st.session_state[f"model_type_{i+1}"]} 
-               for i in range(2)]
-    
-    for i in range(2):
-        if models[i]["endpoint"]=="" or models[i]["key"]=="":
-            missing = True
-    
-    if missing:
-        prompt_container.error("Please fill in endpoint and API Key for both models in the settings tab")
-    else:
-        ctx = get_script_run_ctx()
-        threads = []
-        for i in range(2):
-            thread = threading.Thread(target=ai_inference_stream, args=(ctx,prompt,i))
-            thread.start()
-            threads.append(thread)
-        
-        for t in threads:
-            t.join()
-        
-        if len(threadErrors)>0:
-            for error in threadErrors:
-                headcol2.error(f"**Error:** {error[0]} in thread {error[1]}")
-        
-        else:
-            for i in range(2):
-                if results[i] is not None:
-                    with columns[i]:
-                        st.write(f"Time to first token: {results[i]['time_first_token'].total_seconds()} seconds")
-                        st.write(f"Output Tokens: {results[i]['output_tokens']}")
-                        st.write(f"Tokens per second: {float(results[i]['output_tokens'])/results[i]['total_time'].total_seconds()}")
 
 setup = st.sidebar
 setup.subheader("Settings")
@@ -170,9 +138,12 @@ with setup.form("Model settings"):
         with collist[1]:
             with st.popover(f"Settings"):
                 st.text_input(f"Display Name", key=f"model_name_{i+1}")
-                st.text_input("Endpoint URL", key=f"url_{i+1}")
-                st.text_input("API Key", type="password", key=f"key_{i+1}")
-                st.text_input("Deployment (AOAI Only)", key=f"deployment_{i+1}", help="Deployment name for AOAI models")
+                st.text_input("Endpoint URL", key=f"url_{i+1}",value="https://vh-oai-east2.openai.azure.com")
+                st.text_input("API Key", type="password", key=f"key_{i+1}", value="57ad3c90d0f441fdbd7dffda4ae6601d")
+                st.text_input("Deployment (AOAI Only)", 
+                              key=f"deployment_{i+1}", 
+                              help="Deployment name for AOAI models",
+                              value="gpt-4o-global")
 
     
     st.text_area("System Prompt", value="You are a helpful AI Assistant", key="system_prompt")
@@ -184,6 +155,18 @@ help = """Supported models are those available on Azure, this includes:
         \n - Azure AI serverless inference models (LLama2, Cohere, Phi-3 etc.)
         \n - Azure managed endpoints (**ONLY** Llama3 instruct, Phi-3 family, Mixtral family)"""
 
+setup.subheader("(Optional) Image Input")
+uploaded_file = setup.file_uploader("Use Image input", type=["jpg", "jpeg", "png"])
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    # Convert bytes to base64
+    base64_data = base64.b64encode(bytes_data).decode('utf-8')
+    base64_url = f"data:image/png;base64,{base64_data}" 
+    st.session_state["image"] = base64_url
+    setup.image(uploaded_file, use_column_width=True)
+else:
+    st.session_state["image"] = None
 setup.markdown(help)
 
 if submit:
@@ -203,5 +186,58 @@ css = '''
     }
 </style>
 '''
-st.markdown(css, unsafe_allow_html=True)  
+st.markdown(css, unsafe_allow_html=True) 
+
+timers = [timer1, timer2]
+containers = [container1, container2]
+columns = [col1, col2]
+missing = False
+bytes_data = None
+
+if prompt := st.chat_input("Enter a prompt"):
+    prompt_container.empty()
+    
+    if st.session_state.get('image') is not None:
+        pccol1, pccol2 = prompt_container.columns([1,3])
+        pccol2.image(uploaded_file, width=150)
+        pccol1.markdown(f"**Prompt:** {prompt}")
+    else:
+        prompt_container.markdown(f" **Prompt:** {prompt}")
+    models = [{"endpoint":st.session_state[f"url_{i+1}"],
+               "key":st.session_state[f"key_{i+1}"],
+               "deployment":st.session_state[f"deployment_{i+1}"],
+               "model":st.session_state[f"model_{i+1}"],
+               "model_type":st.session_state[f"model_type_{i+1}"]} 
+               for i in range(2)]
+    
+    for i in range(2):
+        if models[i]["endpoint"]=="" or models[i]["key"]=="":
+            missing = True
+    
+    
+    if missing:
+        prompt_container.error("Please fill in endpoint and API Key for both models in the settings tab")
+    else:
+        ctx = get_script_run_ctx()
+        threads = []
+        for i in range(2):
+            thread = threading.Thread(target=ai_inference_stream, args=(ctx,prompt,i, base64_url))
+            thread.start()
+            threads.append(thread)
+        
+        for t in threads:
+            t.join()
+        
+        if len(threadErrors)>0:
+            for error in threadErrors:
+                headcol2.error(f"**Error:** {error[0]} in thread {error[1]}")
+        
+        else:
+            for i in range(2):
+                if results[i] is not None:
+                    with columns[i]:
+                        st.write(f"Time to first token: {results[i]['time_first_token'].total_seconds()} seconds")
+                        st.write(f"Output Tokens: {results[i]['output_tokens']}")
+                        st.write(f"Tokens per second: {float(results[i]['output_tokens'])/results[i]['total_time'].total_seconds()}")
+        
     
